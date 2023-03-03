6ba7041c71fafbff94f005c069e2fec61b3fbb09
Muhamad Fauzan Putra <business.mfauzanptra@gmail.com>
2023-03-03 14:06:31 +0700

    add database configuration

---
+++ config/config.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Config struct {
+	DBUser string
+	DBPass string
+	DBHost string
+	DBPort int
+	DBName string
+}
+
+func ReadConfig() *Config {
+	err := godotenv.Load("local.env")
+	if err != nil {
+		fmt.Println("error read env file: ", err.Error())
+		return nil
+	}
+
+	var res Config
+	res.DBUser = os.Getenv("DBUSER")
+	res.DBPass = os.Getenv("DBPASS")
+	res.DBHost = os.Getenv("DBHOST")
+	readData := os.Getenv("DBPORT")
+	res.DBPort, err = strconv.Atoi(readData)
+	if err != nil {
+		fmt.Println("error convert: ", err.Error())
+		return nil
+	}
+	res.DBName = os.Getenv("DBNAME")
+	return &res
+}
+
+func ConnectSQL(c Config) *gorm.DB {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", c.DBUser, c.DBPass, c.DBHost, c.DBPort, c.DBName)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("database connection error: ", err.Error())
+	}
+
+	return db
+}
